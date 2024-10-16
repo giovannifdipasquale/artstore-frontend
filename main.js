6dd2243dc39e27f0d71cda45809a409f4a16307b
giovannifdipasquale <giovanni.francesco97@gmail.com>
2024-10-16 11:15:23 +0200

    aggiunta: 1. numeri che incrementano 2. animation on scrolls 3. collegamento tra filtri (products page) 4. carousel slider

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+AOS.init();
 // SELECTORS
 const navbar = document.querySelector("nav");
+const piecesNumber = document.querySelector(".pieces-number");
+const costumersNumber = document.querySelector(".costumers-number");
 const numbers = document.querySelectorAll(".numbers");
 // VARIABLES
 
@@ -16,7 +19,56 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("navbar-scroll");
   }
 });
-window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  
+
+
+// ASYNC FUNCTIONS
+// function create interval
+function createInterval(element, maxValue, interval) {
+  let index = 0;
+  let newInterval = setInterval(() => {
+    if (index < maxValue) {
+      index++;
+      element.innerText = index;
+    }
+    else {
+      clearInterval(newInterval);
+    }
+  }, interval)
+}
+
+
+// set interval pieces
+
+let isIntersected = true;
+// intersection observer
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting && isIntersected == true) {
+      // set intervals
+      createInterval(costumersNumber, 50, 50);
+      createInterval(piecesNumber, 40, 100);
+      isIntersected = false;
+      let timeout = setTimeout(() => {
+        isIntersected = true;
+      }, 10000);
+    }
+  })
+})
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
+
+observer.observe(costumersNumber);
